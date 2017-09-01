79b7b069c351d9906581548a8148fe13fa6333e5
RyanFleming <ryanwfleming@gmail.com>
2017-09-01 10:28:08 -0700

    Update to read standard input in canonical mode

--- src/kilo.c
+++ src/kilo.c
@@ -4,6 +4,9 @@
 // Description: Version of antirez's kilo text editor written in C.
 // Implements basic features as well as syntax highlighting and search.
 //
+#include <unistd.h>
 int main() {
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1);
   return 0;
 }
