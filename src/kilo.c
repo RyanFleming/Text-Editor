8c4191bcd354635b1a80ad4a6349ad61b992ba35
RyanFleming <ryanwfleming@gmail.com>
2017-09-01 10:54:03 -0700

    Update disable raw ode at exit

--- src/kilo.c
+++ src/kilo.c
@@ -4,22 +4,34 @@
 // Description: Version of antirez's kilo text editor written in C.
 // Implements basic features as well as syntax highlighting and search.
 //
+
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-//
-// Description: Enables raw mode.
-//
-void EnableRawMode() {
+
+struct termios orig_termios;
+
+void disableRawMode() {
+  //Sets terminal to original state
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
   //Read terminal's current attributes into a struct
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  //Turn off ECHO feature in struct
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  //Turn off ECHO feature in copy of struct
   raw.c_lflag &= ~(ECHO);
+
   //Pass the modified struct to write the new terminal attributes.
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 int main() {
-  EnableRawMode();
+  enableRawMode();
+
   //Read 1 byte from standard input into the variable c
   //until there are no more bytes to read.
   //Quit program when it reads 'q' character.
