96cdca490945956946fbaa938c6e4ef8c5e3785a
RyanFleming <ryanwfleming@gmail.com>
2017-09-01 14:32:30 -0700

    Update center welcome message

--- src/kilo.c
+++ src/kilo.c
@@ -157,6 +157,13 @@ void editorDrawRows(struct abuf *ab) {
       int welcomelen = snprintf(welcome, sizeof(welcome),
         "Kilo editor -- version %s", KILO_VERSION);
       if (welcomelen > E.screencols) welcomelen = E.screencols;
+      //Padding to center message
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
       abAppend(ab, welcome, welcomelen);
     } else {
       //Add '~' to left hand side like vime
