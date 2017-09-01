a6d1d442754c3c8e9cc3ede802a3bea42da28e36
RyanFleming <ryanwfleming@gmail.com>
2017-09-01 13:05:00 -0700

    Update clear screen on exit

--- src/kilo.c
+++ src/kilo.c
@@ -26,6 +26,10 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  //Clear screen 
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  
   perror(s);
   exit(1);
 }
@@ -89,6 +93,9 @@ void editorProcessKeypress() {
 
   switch (c) {
     case CTRL_KEY('q'):
+      //Clear screen
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
