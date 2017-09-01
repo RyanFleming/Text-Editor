7508f20f638c20b8049b8d6a9e81565e48caaec1
RyanFleming <ryanwfleming@gmail.com>
2017-09-01 13:16:21 -0700

    Update find terminal window size

--- src/kilo.c
+++ src/kilo.c
@@ -12,6 +12,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -78,6 +79,19 @@ char editorReadKey() {
     if (nread == -1 && errno != EAGAIN) die("read");
   }
   return c;
+}
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  //place the number of columns and rows into winsize struct
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
 }
 
 /*** output ***/
