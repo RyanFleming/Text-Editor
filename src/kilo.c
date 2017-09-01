92c6a3bd4fb916724cb8b59d920e32bdfcce837c
RyanFleming <ryanwfleming@gmail.com>
2017-09-01 10:18:54 -0700

    Update source directory and main file

---
+++ src/kilo.c
@@ -0,0 +1,9 @@
+//
+// Author: Ryan Fleming
+// Date: 9/1/17
+// Description: Version of antirez's kilo text editor written in C.
+// Implements basic features as well as syntax highlighting and search.
+//
+int main() {
+  return 0;
+}
